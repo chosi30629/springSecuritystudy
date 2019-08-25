b648ad996b35e65595c88128363ae496a1664b68
조성일 <chosi30629@gmail.com>
2019-08-25 14:46:27 +0900

    Authentication과 SecurityContextHolder

--- src/main/java/me/chosi/demospringsecurityform/form/SampleService.java
+++ src/main/java/me/chosi/demospringsecurityform/form/SampleService.java
@@ -5,6 +5,7 @@ import me.chosi.demospringsecurityform.account.AccountContext;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Collection;
@@ -21,9 +22,11 @@ public class SampleService {
         boolean authenticated = authentication.isAuthenticated();
         */
 
-        Account account = AccountContext.getAccount();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         System.out.println("==================");
-        System.out.println(account.getUsername());
+        System.out.println(authentication);
+        System.out.println(userDetails.getUsername());
 
     }
 
